9fd2c02817f049578f517169150369fa337ced7d
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-13 00:10:43 +0200

    Create api end points

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < Api::V1::ApiController
+    def index
+      render json: User.all
+    end
+  
+    def show
+      @user = User.find(params[:id])
+      render json: { success: true, data: { user: @user, message: 'User found' } }
+    end
+end
