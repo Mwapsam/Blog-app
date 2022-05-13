487059186ac4967c49a31e51f4caf8106399723c
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-13 18:35:37 +0200

    Remove unwanted file

--- otto.sh
+++
@@ -1,7 +0,0 @@
-git add .
-read -p "Enter commit message: " commit_message
-git commit -m "$commit_message"
-BRANCH=$(git describe --contains --all HEAD)
-git pull --rebase origin "$BRANCH"
-git push origin "$BRANCH"
-exit
