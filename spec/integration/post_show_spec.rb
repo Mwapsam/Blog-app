be00251a7ff836bb3e7339c5093f1cbfcb8806a9
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-14 12:47:55 +0200

    Fix errors on post show tests

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Posts show page', type: :feature do
   before(:each) do
-    @user = User.create(name: 'Mwape', photo: 'profile.jpg', bio: 'Developer from Zambia', email: 'test@email.com',
-                        password: 'password', confirmed_at: Time.now)
+    User.destroy_all
+    @user = User.create(name: 'Mwape', photo: 'https://i.kinja-img.com/gawker-media/image/upload/t_original/ijsi5fzb1nbkbhxa2gc1.png', bio: 'Developer from Zambia', email: 'test@email.com',
+                        password: 'password', confirmed_at: Time.now, role: 'admin',  posts_counter: 0)
     @user1 = User.create(name: 'Lungu', photo: 'profile.jpg', bio: 'Developer from SA', email: 'test1@email.com',
                          password: 'password', confirmed_at: Time.now)
-    @post = Post.create(author: @user, title: 'My title', text: 'My text')
-    @comment = Comment.create(text: 'My first comment', author: @user, post_id: @post.id)
-    @comment = Comment.create(text: 'My second comment', author: @user1, post_id: @post.id)
-    @like = Like.create(author: @user, post_id: @post.id)
+    Post.create(title: 'My title', text: 'My text', author_id: @user.id, likes_counter: 0, comments_counter: 0)
+    @comment = Comment.create(text: 'My first comment', author: @user, id: Post.first.id)
+    @comment = Comment.create(text: 'My second comment', author: @user1, id: Post.first.id)
+    @like = Like.create(author: @user, id: Post.first.id)
+    
     visit new_user_session_path
     fill_in 'Email', with: 'test@email.com'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
-    visit user_post_path(user_id: @post.author_id, id: @post.id)
+    visit "/users/#{User.first.id}/posts/#{Post.first.id}"
   end
 
   describe 'Specs for view posts#show' do
@@ -23,7 +25,7 @@ RSpec.describe 'Posts show page', type: :feature do
     end
 
     it 'Can see who wrote the post' do
-      expect(page).to have_content 'Mwape'
+      expect(page).to have_content('Mwape')
     end
 
     it 'Can see how many comments it has' do
