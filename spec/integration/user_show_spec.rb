af19639e4aac0f4699cef90e74e96844e51fc304
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-14 07:20:54 +0200

    Fix linter errors

--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature 'Testing user show page', type: :feature do
-before(:each) do
+  before(:each) do
     @first = User.create(name: 'John', photo: 'profile.jpg', bio: 'Teacher from Mexico.', email: 'john@gmail.com',
                          password: 'johnsecret', confirmed_at: Time.now, posts_counter: 3, role: 'admin')
 
     Post.create(title: 'Command on Windows', text: 'How to add, commit and push to git',
-                        author_id: @first, created_at: Time.now, updated_at: Time.now)
-    
+                author_id: @first, created_at: Time.now, updated_at: Time.now)
+
     Post.create(title: 'Command on Linux', text: 'How to add, commit and push to git',
-                        author_id: @first, created_at: Time.now, updated_at: Time.now)
+                author_id: @first, created_at: Time.now, updated_at: Time.now)
 
     Post.create(title: 'Command on Mac', text: 'How to add, commit and push to git',
-                        author_id: @first, created_at: Time.now, updated_at: Time.now)
+                author_id: @first, created_at: Time.now, updated_at: Time.now)
 
     visit user_session_path
 
@@ -24,32 +24,32 @@ before(:each) do
     click_button 'Log in'
   end
 
-    background { visit user_path(@first.id) }
+  background { visit user_path(@first.id) }
 
-    scenario "I can see the user's username" do
-        expect(page).to have_content('John')
-    end
+  scenario "I can see the user's username" do
+    expect(page).to have_content('John')
+  end
 
-    scenario "I can see the user's profile picture" do
-        expect(page.first('img')['src']).to have_content '/assets/profile-placeholder-15235acc91950fc6e35d44cd5337db6ca48c73d53e4aa0f7f8dec750ad5242ce.png'
-    end
+  scenario "I can see the user's profile picture" do
+    expect(page.first('img')['src']).to have_content '/assets/profile-placeholder-15235acc91950fc6e35d44cd5337db6ca48c73d53e4aa0f7f8dec750ad5242ce.png'
+  end
 
-    scenario "If the user can see the number of posts written" do
-        expect(page).to have_content('Wrote 3 posts')
-    end
+  scenario 'If the user can see the number of posts written' do
+    expect(page).to have_content('Wrote 3 posts')
+  end
 
-    scenario "I can see the user's bio" do
-        expect(page).to have_content('Teacher from Mexico.')
-    end
+  scenario "I can see the user's bio" do
+    expect(page).to have_content('Teacher from Mexico.')
+  end
 
-    scenario "I can see the user's posts" do
-        expect(page).to have_content('Command on Windows')
-        expect(page).to have_content('Command on Linux')
-        expect(page).to have_content('Command on Mac')
-    end
+  scenario "I can see the user's posts" do
+    expect(page).to have_content('Command on Windows')
+    expect(page).to have_content('Command on Linux')
+    expect(page).to have_content('Command on Mac')
+  end
 
-    scenario "I can see a button that lets me view all of a user's posts" do
-        click_link('All posts')
-        expect(current_path).to eq user_posts_path(@first)
-    end
+  scenario "I can see a button that lets me view all of a user's posts" do
+    click_link('All posts')
+    expect(current_path).to eq user_posts_path(@first)
+  end
 end
