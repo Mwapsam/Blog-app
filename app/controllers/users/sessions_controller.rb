0afc8ed6b176d743e6ade53b58ddf4cc64e64930
uae <aniekaudo@yahoo.com>
2022-05-14 14:57:43 +0100

    :alien: Session controller
    Creating log out failure method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,6 +10,9 @@ class Users::SessionsController < Devise::SessionsController
     def log_out_success
       render json: { message: 'Logged out.' }, status: :ok
     end
+    def log_out_failure
+      render json: { message: 'Logged out failure.' }, status: :unauthorized
+    end
   
     
 end
