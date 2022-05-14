5a6ad4d48782ad439eff9b1dc9b3d010134bcb38
uae <aniekaudo@yahoo.com>
2022-05-14 14:54:54 +0100

    :alien: Respond path method
    creating respond method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
     respond_to :json
     private
+    def respond_with(_resource, _opts = {})
+      render json: { message: 'Logged in.' }, status: :ok
+    end
+  
     
 end
